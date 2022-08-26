bd4ee26a438b49ef409d0c3b541edd8cda4840f0
double <21sunrin077@sunrint.hs.kr>
2022-08-26 22:06:54 +0900

    docs:00

---
+++ helper.js
@@ -0,0 +1,16 @@
+document.querySelector();
+
+document.querySelectorAll();
+
+// https://ko.javascript.info/
+// 모던 자바스크립트 튜토리얼
+
+// https://www.w3schools.com/js/
+// W3School 자바스크립트 튜토리얼
+
+
+// https://www.w3schools.com/js/js_htmldom.asp
+// W3School DOM
+
+// https://www.w3schools.com/cssref/css_selectors.asp
+// W3School CSS selector
--- recordingLoL.js
+++ recordingLoL.js
@@ -1,44 +1,45 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-// const crawler = async (name) => {
-//   try {
-//     const browser = await puppeteer.launch({ headless: false });
-//     const page = await browser.newPage();
-//     await page.setViewport({
-//       width: 1920,
-//       height: 1080,
-//     });
-//     await page.goto('https://poro.gg/summoner/kr/'+name);
+const crawler = async (name) => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+    });
+    await page.goto('https://poro.gg/summoner/kr/'+name);
     
-//     let result = [];
-//     while(result.length < 20) {
-//       const text = await page.evaluate(() => {
-//         const Feed = document.querySelector('div.flex.flex-row.items-center > strong > span').textContent;
-//         return Feed;
-//       });
-//       result.push(text);
-//       await page.evaluate(() => {
-//         let Feed = document.querySelector('div.flex.flex-row.items-center > strong > span');
-//         Feed.parentNode.removeChild(Feed);
-//         Feed = document.querySelector('div.flex.flex-row.items-center > strong > span');
-//         Feed.parentNode.removeChild(Feed);
-//       });
-//     }
-//     console.log(result);
-//     let win = 0;
-//     result.forEach((r, i) => {
-//       if (r == "승리") {
-//         win++;
-//       }
-//     })
-//     let rate = win/20 *100;
-//     console.log(name+'의 최근 20게임 승률은'+rate+'%입니다.');
-//     await browser.close();
-//   } catch (err) {
-//     console.error(err);
-//   }
+    let result = [];
+    while(result.length < 20) {
+      const text = await page.evaluate(() => {
+        const Feed = document.querySelector('div.flex.flex-row.items-center > strong > span').textContent;
+        // .page-container  div.hidden  div.items-center strong span 대체가능
+        return Feed;
+      });
+      result.push(text);
+      await page.evaluate(() => {
+        let Feed = document.querySelector('div.flex.flex-row.items-center > strong > span');
+        Feed.parentNode.removeChild(Feed);
+        Feed = document.querySelector('div.flex.flex-row.items-center > strong > span');
+        Feed.parentNode.removeChild(Feed);
+      });
+    }
+    console.log(result);
+    let win = 0;
+    result.forEach((r, i) => {
+      if (r == "승리") {
+        win++;
+      }
+    })
+    let rate = win/20 *100;
+    console.log(name+'의 최근 20게임 승률은'+rate+'%입니다.');
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
 
   
-// }
+}
 
-// crawler('더블은못날아');
+crawler('더블은못날아');
